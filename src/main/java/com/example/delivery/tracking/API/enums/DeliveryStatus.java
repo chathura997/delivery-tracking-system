d0715c5f90bacc0f61b0051cbf5c2ffaa504fbd6
chathura <chathuradinushka97@gmail.com>
2025-10-13 16:40:39 +0530

    Update DeliveryStatus

--- src/main/java/com/example/delivery/tracking/API/enums/DeliveryStatus.java
+++ src/main/java/com/example/delivery/tracking/API/enums/DeliveryStatus.java
@@ -1,5 +1,5 @@
 package com.example.delivery.tracking.API.enums;
 
 public enum DeliveryStatus {
-    PENDING, ASSIGNED, PICKED_UP, ON_ROUTE, DELIVERED
+    PENDING, ASSIGNED, PICKED_UP, ON_ROUTE, DELIVERED,CANCELLED
 }
