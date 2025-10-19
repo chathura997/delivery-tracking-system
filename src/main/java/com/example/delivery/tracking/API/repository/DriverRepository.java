4693c939ec2e17ac6bbf0a592075d37576ac8e6d
chathura <chathuradinushka97@gmail.com>
2025-10-19 20:05:36 +0530

    add new method

--- src/main/java/com/example/delivery/tracking/API/repository/DriverRepository.java
+++ src/main/java/com/example/delivery/tracking/API/repository/DriverRepository.java
@@ -1,9 +1,12 @@
 package com.example.delivery.tracking.API.repository;
 
 import com.example.delivery.tracking.API.entity.Driver;
+import com.example.delivery.tracking.API.enums.DriverStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 public interface DriverRepository extends JpaRepository<Driver,Long> {
+    List<Driver> findByStatus (DriverStatus status);
 }
