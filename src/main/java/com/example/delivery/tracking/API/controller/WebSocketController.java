bbbe02a5fb9e1c5635db6c55fb72f47b74772e69
chathura <chathuradinushka97@gmail.com>
2025-11-04 14:04:27 +0530

    add web sockets

---
+++ src/main/java/com/example/delivery/tracking/API/controller/WebSocketController.java
@@ -0,0 +1,16 @@
+package com.example.delivery.tracking.API.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+@Slf4j
+@Controller
+public class WebSocketController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) {
+        log.info("Received message: {}", message);
+        return "Hello, " + message + "!";
+    }
+}
